a3e0becc27541c9e7e746f9e5caddf2e71051f25
yeonupark <idepix@naver.com>
2023-08-11 23:35:16 +0900

    Initial Commit

---
+++ Media Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media Project
+//
+//  Created by 마르 on 2023/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
